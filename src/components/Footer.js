acfe7e1aa3b3cfdf3b5ef58a3ec789444bebffc6
VGiktara27 <curiousvidushi27@gmail.com>
2025-03-19 22:45:58 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,45 @@
 import Link from "next/link";
-import { FaLinkedinIn ,FaGithub ,FaInstagram,FaGoogleScholar} from "react-icons/fa6";
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaInstagram,
+  FaGoogleScholar,
+} from "react-icons/fa6";
 import { CiMail } from "react-icons/ci";
+
 export default function Footer() {
   return (
     <>
-    <hr/>
-    <footer className="bg-white-800 text-black w-full py-4 max-w-4xl mx-auto">
-    <div className="flex flex-col sm:flex-row justify-between items-center">
       
-      {/* Left section: About */}
-      <div className="text-center sm:text-left mb-4 sm:mb-0">
-        <h2 className="text-xl font-bold">This website is open-source on GitHub</h2>
-      </div>
-
-      {/* Right section: Social Media Icons */}
-      <div className="flex justify-center sm:justify-start space-x-4 mt-6 sm:mt-0">
-        <a href="https://www.linkedin.com/in/vidushi-gandhi27/" ><FaLinkedinIn size ='30'/></a>
-        <a href="https://github.com/VGandhi27"><FaGithub size ='30' color = ''/></a>
-        <a href="https://github.com/Vidushi-Gandhi"><CiMail size ='30'/></a>
-        <a href="https://www.instagram.com/vgandhi72/"><FaInstagram size ='30'/></a>
-        <a href="https://scholar.google.com/citations?user=pHa-9woAAAAJ&hl=en"><FaGoogleScholar size ='30'/></a>
-      </div>
+      <footer className="bg-gradient-to-r from-[#1a0b1f] to-[#430c4d] text-white w-full py-4 px-4">
+  <div className="flex flex-col sm:flex-row justify-between items-center max-w-6xl mx-auto">
+    {/* Left section: About */}
+    <div className="text-center sm:text-left mb-4 sm:mb-0">
+      <h2 className="text-xl font-bold">This website is open-source on GitHub</h2>
     </div>
- 
+
+    {/* Right section: Social Media Icons */}
+    <div className="flex justify-center sm:justify-start space-x-4 mt-6 sm:mt-0">
+      <a href="https://www.linkedin.com/in/vidushi-gandhi27/" target="_blank">
+        <FaLinkedinIn size="30" />
+      </a>
+      <a href="https://github.com/VGandhi27" target="_blank">
+        <FaGithub size="30" />
+      </a>
+      <a href="mailto:vidushi@example.com" target="_blank">
+        <CiMail size="30" />
+      </a>
+      <a href="https://www.instagram.com/vgandhi72/" target="_blank">
+        <FaInstagram size="30" />
+      </a>
+      <a href="https://scholar.google.com/citations?user=pHa-9woAAAAJ&hl=en" target="_blank">
+        <FaGoogleScholar size="30" />
+      </a>
+    </div>
+  </div>
 </footer>
-</>
+
+
+    </>
   );
 }
--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,7 @@ import "./globals.css"
 export default function About_Com() {
   return (
     
-    <div className="max-w-4xl mx-auto p-6 text-left mt-10" style={{marginTop: '5rem' }}>
+    <div className=" min-h-[75vh] max-w-4xl mx-auto p-6 text-left mt-10" style={{marginTop: '5rem' ,marginBottom: '2rem'}}>
      <div className="flex justify-between items-center">
     <h1 className="text-4xl font-bold mt-2 mb-6">About Me</h1>
     <a
